c3c47264efed7ad4c4c1e97b0ce06cf2dc3fac70
Tae wook <laputa99999@gmail.com>
2022-03-02 13:46:14 +0900

    Add files via upload

--- auto_report.bat
+++ auto_report.bat
@@ -1,1 +1,3 @@
-python gen_report.py 2022/1/2 https://sites.google.com/site/bimprinciple/in-the-news/gieob-uidijiteoljeonhwanguhyeon out.docx 2000 300 2000
+python gen_report.py 2022/1/2 https://sites.google.com/site/bimprinciple/in-the-news/gieob-uidijiteoljeonhwanguhyeon r1.docx 2000 300 2000
+python gen_report.py 2022/1/3 http://daddynkidsmakers.blogspot.com/2022/02/pdal-3.html r2.docx 1000 200 50
+python gen_report.py 2022/1/4 "computer science" r3.docx 100 300 50
--- gen_report.py
+++ gen_report.py
@@ -1,8 +1,7 @@
 # Purpose: Generate research & meeting report automatically from website or blog
 # Date: 2022.3.1
-# Reference
-#   https://www.scrapingbee.com/blog/crawling-python/
-#   https://python-docx.readthedocs.io/en/latest/user/install.html
+# Author: mac999
+# 
 
 import logging
 from urllib.parse import urljoin
@@ -19,7 +18,7 @@ logging.basicConfig(
     level=logging.INFO)
 
 class auto_report:
-    _url = ""
+    _urls = []
     _output = ""
     _begin = 0
     _dist = 0
@@ -30,6 +29,16 @@ class auto_report:
     def download_url(self, url):
         return requests.get(url).text
 
+    def searchGoogle(self, query):
+        try:
+            from googlesearch import search
+        except ImportError:
+            print("No module named 'google' found")
+
+        for u in search(query, tld="co.in", num=10, stop=10, pause=2):
+            print(u)
+            self._urls.append(u)
+
     def get_linked_urls(self, url, html):
         soup = BeautifulSoup(html, 'html.parser')
         for link in soup.find_all('a'):
@@ -38,8 +47,15 @@ class auto_report:
                 path = urljoin(url, path)
             yield path
 
+    def is_empty_content(self, txt, ratio = 0.7):
+        space = txt.count(' ')    
+        r = 1.0 - float(space) / float(len(txt))
+        if r < ratio:
+            return True
+        return False
+
     def generate_body(self, html, begin = 2000, dist = 500, step = 2000, cnt = 1):
-        print("Research Note")
+        print("\nResearch Note")
         soup = BeautifulSoup(html, 'html.parser')
 
         body = ""
@@ -57,13 +73,16 @@ class auto_report:
             current = 0
             while (b + dist + step) < len(t):
                 t2 = t[b: b + dist]
+                b = b + dist + step
+
+                if self.is_empty_content(t2):
+                    continue
 
                 if current >= len(self._body_title):
-                    current = 0
+                    break
                 t2 = self._body_title[current] + "\n" + t2 + "...\n\n"
                 print(t2)
                 body = body + t2
-                b = b + dist + step
                 print("begin = ", b)
                 current = current + 1
 
@@ -78,44 +97,38 @@ class auto_report:
 
     def createWord(self, fname = "out.docx", body = "Hello world!"):
         document = Document()
-        document.add_heading('연구노트', 0)
+        document.add_heading('Research Note', 0)
 
-        table = document.add_table(rows=3, cols=5)
+        table = document.add_table(rows=3, cols=4)
         hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = '현황'
-        hdr_cells[1].text = '소속'
-        hdr_cells[2].text = self._header[0]
-        hdr_cells[3].text = '직급'
-        hdr_cells[4].text = self._header[1]
+        hdr_cells[0].text = 'Department'
+        hdr_cells[1].text = self._header[0]
+        hdr_cells[2].text = 'Position'
+        hdr_cells[3].text = self._header[1]
         hdr_cells = table.rows[1].cells
-        hdr_cells[1].text = '사번'
-        hdr_cells[2].text = self._header[2]
-        hdr_cells[3].text = '성명'
-        hdr_cells[4].text = self._header[3]
+        hdr_cells[0].text = 'ID'
+        hdr_cells[1].text = self._header[2]
+        hdr_cells[2].text = 'Name'
+        hdr_cells[3].text = self._header[3]
         hdr_cells = table.rows[2].cells
-        hdr_cells[1].text = '일자'
-        hdr_cells[2].text = self._date
-        hdr_cells[3].text = '근무시간/장소'
-        hdr_cells[4].text = self._header[4]
+        hdr_cells[0].text = 'Date'
+        hdr_cells[1].text = self._date
+        hdr_cells[2].text = 'Place'
+        hdr_cells[3].text = self._header[4]
 
-        table = document.add_table(rows=3, cols=2)
+        table = document.add_table(rows=2, cols=1)
         hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = '주요수행업무'
-        hdr_cells[1].text = body
+        hdr_cells[0].text = 'Note'
         hdr_cells = table.rows[1].cells
-        hdr_cells[0].text = '이동현황'
-        hdr_cells[1].text = '-'
-        hdr_cells = table.rows[2].cells
-        hdr_cells[0].text = '기타사항'
-        hdr_cells[1].text = '-'
+        hdr_cells[0].text = body
 
         table = document.add_table(rows=1, cols=5)
         hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = '확인'
-        hdr_cells[1].text = '부서(직책)'
+        hdr_cells[0].text = 'Confirmation'
+        hdr_cells[1].text = 'Position'
         hdr_cells[2].text = self._header[0]
-        hdr_cells[3].text = '성명'
-        hdr_cells[4].text = self._header[3] + ' (서명)'
+        hdr_cells[3].text = 'Name'
+        hdr_cells[4].text = self._header[3] + ' (Sign)'
 
         # document.add_page_break()
         document.save(fname)
@@ -127,12 +140,18 @@ if __name__ == '__main__':
     c = auto_report()
     if len(sys.argv) >= 7:
         c._date = sys.argv[1]
-        c._url = sys.argv[2]
+        c._urls.append(sys.argv[2])
+        if sys.argv[2].find('http') < 0:
+            print("Knowledge searching mode...")
+            c._urls.clear()
+            c.searchGoogle("computer science")
+
         c._output = sys.argv[3]
         c._begin = int(sys.argv[4])
         c._dist = int(sys.argv[5])
         c._step = int(sys.argv[6])
 
+    print("\nReading config file...")
     index = 0
     with open("config.csv", 'r', encoding = 'UTF-8') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
@@ -143,9 +162,10 @@ if __name__ == '__main__':
                 c._body_title = row 
             index = index + 1
 
+    print("\nProcessing...")
     print(c._header)        
     print(c._body_title)        
 
-    html = c.download_url(c._url)
+    html = c.download_url(c._urls[0])
     body = c.generate_body(html, c._begin, c._dist, c._step)
     c.createWord(c._output, body)
